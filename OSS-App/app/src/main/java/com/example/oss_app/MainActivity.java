72ad0a88d51a4f8921a032899dd644d006b445b9
gnwjd309 <fujeong15@naver.com>
2021-05-08 19:11:52 +0900

    Add TTS output

--- OSS-App/app/src/main/java/com/example/oss_app/MainActivity.java
+++ OSS-App/app/src/main/java/com/example/oss_app/MainActivity.java
@@ -8,15 +8,24 @@ import android.widget.Button;
 
 public class MainActivity extends Activity {
 
-    Button sign_up_btn, sign_in_btn;
+    Button sign_up_btn, sign_in_btn, Start;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        Start = findViewById(R.id.Start);
         sign_up_btn = findViewById(R.id.sign_up_btn);
         sign_in_btn = findViewById(R.id.sign_in_btn);
+
+        Start.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, soundPlay.class);
+                startActivity(intent);
+            }
+        });
 
         sign_up_btn.setOnClickListener(new View.OnClickListener() {
             @Override
